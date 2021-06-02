509fc09f25cdf7a91fa7b92816dd23ed635aca8c
Oshomah <oshomah26@gmail.com>
2021-06-02 21:34:52 +0100

    sayMe.hs

--- Documents/Osh/code/Haskell/sayMe.hs
+++ Documents/Osh/code/Haskell/sayMe.hs
@@ -8,4 +8,4 @@ myLast [] = error "can't find last element of an empty list!"
 myLast [_,x] = x 
 myLast (_:xs) = myLast xs 
 
-
+boomBangs xs = [if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
