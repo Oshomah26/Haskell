b1029520759406946dcee1bd58a6789c274cfb16
Oshomah <oshomah26@gmail.com>
2021-06-08 09:03:10 +0100

    BMI update

---
+++ sayMe.hs
@@ -0,0 +1,23 @@
+[33mcommit 509fc09f25cdf7a91fa7b92816dd23ed635aca8c[m[33m ([m[1;36mHEAD -> [m[1;32mLesson1[m[33m, [m[1;31morigin/Lesson1[m[33m)[m
+Author: Oshomah <oshomah26@gmail.com>
+Date:   Wed Jun 2 21:34:52 2021 +0100
+
+    sayMe.hs
+
+[33mcommit 375daf04b6c7123f6a292d42334122534ce7e9ed[m
+Author: Oshomah <oshomah26@gmail.com>
+Date:   Tue Jun 1 09:32:40 2021 +0100
+
+    sayMe.hs
+
+[33mcommit c5651db8e7c5d8dce5646bbb50a746e1893c29b0[m[33m ([m[1;31morigin/master[m[33m, [m[1;32mmaster[m[33m)[m
+Author: Oshomah <oshomah26@gmail.com>
+Date:   Tue Jun 1 09:20:21 2021 +0100
+
+    Add Haskell files up
+
+[33mcommit 7f10e58f41623f3b041e4719ef44ea9c91549f60[m
+Author: Oshomah <oshomah26@gmail.com>
+Date:   Tue Jun 1 07:55:34 2021 +0100
+
+    create index.html
