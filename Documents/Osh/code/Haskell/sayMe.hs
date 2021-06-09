3a11615d86a23a5239912eded000d2e66d4d4c24
Oshomah <oshomah26@gmail.com>
2021-06-09 07:40:14 +0100

    BMI updated

--- Documents/Osh/code/Haskell/sayMe.hs
+++ Documents/Osh/code/Haskell/sayMe.hs
@@ -8,4 +8,11 @@ myLast [] = error "can't find last element of an empty list!"
 myLast [_,x] = x 
 myLast (_:xs) = myLast xs 
 
-boomBangs xs = [if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+boomBangs xs = [if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x] 
+
+
+bmiTell weight height 
+ | weight / height ^ 2 <= 18.5 = "You're underweight, you emo, you!"
+ | weight / height ^ 2 <= 25.0 = "You're supposedly normal. pfft. I bet your fart stinks!"
+ | weight / height ^ 2 <= 30.0 = "You're fat! Lose some weight, fatty!"
+ | otherwise                   = "You're a whale, congratulations!"
